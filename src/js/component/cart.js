0d095707abc2457139f15b004d710b6b5e9bda42
DavidJJulio <158339997+DavidJJulio@users.noreply.github.com>
2024-05-21 04:40:06 -0500

    feat: :rocket: Proyect done

--- src/js/component/cart.js
+++ src/js/component/cart.js
@@ -1,6 +1,6 @@
 import { LitElement, html, css } from 'lit';
 
-import { getCart, clearCart } from './cart_module.js';
+import { getCart, clearCart, removeFromCart } from './cart_module.js';
 
 export class shoppingCart extends LitElement {
     static properties = {
@@ -18,11 +18,17 @@ export class shoppingCart extends LitElement {
         
     }
     static styles = css`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
     .container {
         display: flex;
         flex-direction: column;
         gap: 20px;
         width: 100%;
+        padding: 10px;
         height: 100%;
         background-color: black;
     }
@@ -32,33 +38,66 @@ export class shoppingCart extends LitElement {
         background-color: gray;
         display: flex;
         align-items: center;
+        justify-content: space-around
     }
-    .img img{
+    img{
         width: 120px;
         height: 140px;
+    }
+    .card div{
+        width: 120px;
+        height: 140px;
+        display: flex;
+        align-items: center;
+    }
+    i{
+        color: white;
+    }
+    .icon {
+        background-color: black;
+    }
+    .trash{
+        width: 120px;
+        height: 140px;
+        background-color: white;
+        border: none;
+    }
+    .vaciar {
+        width: 200px;
+        height: 100px;
     }
     `
 
     render() {
-        if (this.name == "Carrito"){
-          return html`
-            <div class ="container">
-                ${Object.values(this.cart).map(item => html`
-                    <div class ="card">
-                        <div class ="title"><p>${item.title}</p></div>
-                        <div class ="img"><img src ="${item.image}"></div>
-                        <div class ="price"><p>${item.price}</p></div>
-                        <div class ="quantity"><p>${item.quantity}</p></div>
-                    </div>
-                `)}
-                <button @click="${this.clearCart}">Vaciar Carrito</button>
-            </div>
-        `;  
-        }  
+        if (this.name == "Carrito") {
+            return html`
+                <div class ="container">
+                    ${Object.values(this.cart).map(item => html`
+                        <div class ="card">
+                            <div class ="title"><p>${item.title}</p></div>
+                            <div class ="img"><img src ="${item.image}"></div>
+                            <div class ="price"><p>${item.price}$</p></div>
+                            <div class ="quantity"><p>Cantidad: ${item.quantity}</p></div>
+                            <div class ="icon">
+                                <button class ="trash" @click="${() => this.removeFromCart(item.id)}">
+                                    <img src ="./src/assets/img/trash.jpg">
+                                </button>
+                            </div>
+                        </div>
+                    `)}
+                    <button class ="vaciar" @click="${this.clearCart}">Vaciar Carrito</button>
+                </div>
+            `;
+        }
     }
 
     clearCart() {
         localStorage.removeItem('cart');
         this.cart = {};
+    }
+
+    removeFromCart(productId) {
+        removeFromCart(productId); 
+        this.cart = getCart(); 
     }
 }
